d97ee41337bf50d55f32bfeb81c8f7c14baf16d1
junheeLee96 <89452058+junheeLee96@users.noreply.github.com>
2021-11-13 17:44:10 +0900

    Can't find Error of UNDO
    Can't find Error of UNDO

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const undo = document.getElementById("undo");
 
 const INITIAL_COLOR = "2c2c2c";
 const CANVA_SIZE = 700;
@@ -12,96 +12,158 @@ const CANVA_SIZE = 700;
 canvas.width = CANVA_SIZE;
 canvas.height = CANVA_SIZE;
 
-ctx.fillStyle = "white";
-ctx.fillRect(0,0,CANVA_SIZE,CANVA_SIZE);
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+ctx.fillStyle="white";
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
+ctx.fillRect(0, 0, CANVA_SIZE, CANVA_SIZE);
 ctx.lineWidth = 2.5;
 
 
-let painting = false;
 let filling = false;
 
-function stopPainting(){
-    painting=false;
-}
 
-function startPainting(){
-    painting = true;
-}
+let previous = { x: 0, y: 0 };
+let drawing = false;
+let pathsry = [];
+let points = [];
+let mouse = { x: 0, y: 0 };
 
-function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if(!painting){
-        ctx.beginPath();
-        ctx.moveTo(x,y);
-    }else{
-        ctx.lineTo(x,y);
-        ctx.stroke();
-    }
-}
 
-function handleColorClick(event){
-    const color = event.target.style.backgroundColor;
-    ctx.strokeStyle = color;
-    ctx.fillStyle = color;
-}
+// a function to detect the mouse position
 
-function handeRangeChange(event){
-    const size = event.target.value;
-    ctx.lineWidth = size;
-}
 
-function handleModeClick(){
-    if(filling === true){
-        filling = false;
-        mode.innerText = "Fill"
-    }else{
-        filling = true;
-        mode.innerText = "Paint";
-    }
-}
+
 
 function handleCanvasClick(){
-    if(filling){
-        ctx.fillRect(0,0,CANVA_SIZE,CANVA_SIZE);
-    }
+  if(filling){
+      ctx.fillRect(0,0,CANVA_SIZE,CANVA_SIZE);
+  }
 }
-
 function handleCM(event){
-    event.preventDefault();
+  event.preventDefault();
+}
+function handleColorClick(event){
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+function handeRangeChange(event){
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+function handleModeClick(){
+  if(filling === true){
+      filling = false;
+      mode.innerText = "Fill"
+  }else{
+      filling = true;
+      mode.innerText = "Paint";
+  }
 }
 
 function handleSaveClick(){
-    const image = canvas.toDataURL("image/jpeg");
-    const link = document.createElement("a");
-    link.href = image;
-    link.download = "PaintJS";
-    link.click();
+  const image = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS";
+  link.click();
 }
 
-if(canvas){ 
-    canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedown",startPainting)
-    canvas.addEventListener("mouseup",stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click",handleCanvasClick);
-    canvas.addEventListener("contextmenu",handleCM);
+
+
+if (canvas) {
+  
+  function oMousePos(canvas, evt) {
+    let ClientRect = canvas.getBoundingClientRect();
+    return { //objeto
+      x: Math.round(evt.clientX - ClientRect.left),
+      y: Math.round(evt.clientY - ClientRect.top)
+    }
+  }
+
+
+
+  canvas.addEventListener('mousedown', function (e) {
+    drawing = true;
+    previous = { x: mouse.x, y: mouse.y };
+    mouse = oMousePos(canvas, e);
+    points = [];
+    points.push({ x: mouse.x, y: mouse.y })
+  });
+
+  canvas.addEventListener('mousemove', function (e) {
+    if (drawing) {
+      previous = { x: mouse.x, y: mouse.y };
+      mouse = oMousePos(canvas, e);
+      // saving the points in the points array
+      points.push({ x: mouse.x, y: mouse.y })
+      // drawing a line from the previous point to the current point
+      ctx.beginPath();
+      ctx.moveTo(previous.x, previous.y);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.stroke();
+    }
+  }, false);
+
+
+  canvas.addEventListener('mouseup', function () {
+    drawing = false;
+    // Adding the path to the array or the paths
+    pathsry.push(points);
+  }, false);
+
+  function stopPainting(){
+    drawing=false;
 }
+
+
+
+
+  canvas.addEventListener("mouseleave", stopPainting);
+
+  canvas.addEventListener("click",handleCanvasClick);
+  canvas.addEventListener("contextmenu",handleCM);
+
+}
+
+
+function drawPaths() {
+  // delete everything
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // draw all the paths in the paths array
+  pathsry.forEach(path => {
+    ctx.beginPath();
+    ctx.moveTo(path[0].x, path[0].y);
+    for (let i = 1; i < path.length; i++) {
+      ctx.lineTo(path[i].x, path[i].y);
+    }
+    ctx.stroke();
+  })
+}
+
+function Undo() {
+  // remove the last path from the paths array
+  pathsry.splice(-1, 1);
+  // draw all the paths in the paths array
+  drawPaths();
+}
+
+
+undo.addEventListener("click", Undo);
+
 
 Array.from(colors).forEach(color => 
-    color.addEventListener("click",handleColorClick));
-
-
+  color.addEventListener("click",handleColorClick));
 if(range){
-    range.addEventListener("input", handeRangeChange)
+  range.addEventListener("input", handeRangeChange)
 }
-
 if(mode){
-    mode.addEventListener("click",handleModeClick);
+  mode.addEventListener("click",handleModeClick);
+}
+if(saveBtn){
+  saveBtn.addEventListener("click",handleSaveClick);
 }
 
-if(saveBtn){
-    saveBtn.addEventListener("click",handleSaveClick);
-}
